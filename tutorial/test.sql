c5a65af80d3a19c3faf18e9ad60809f2075e0179
hzhou4590 <hzhou4590@gmail.com>
2023-02-11 13:47:02 -0600

    Added new lines for test by Hanhan

--- tutorial/test.sql
+++ tutorial/test.sql
@@ -1,2 +1,6 @@
 --this is a test file for class, just in Tracy branch
-select * from customers where id > 1
+select * from customers
+
+select * from customers where id >10 --Hanhan made this change
+
+select * --Hanhan made 2nd change
